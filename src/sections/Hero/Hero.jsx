26157b8b1e81ed119ec1e8b47020e8910d92d044
Kartik <kartikpoojari29@gmail.com>
2025-05-23 21:53:27 +0530

    resume updated

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,26 +1,26 @@
-import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/kartik-img.png';
-import sun from '../../assets/sun.svg';
-import moon from '../../assets/moon.svg';
-import instagramLight from '../../assets/instagram-light.svg';
-import instagramDark from '../../assets/instagram-dark.svg';
-import githubLight from '../../assets/github-light.svg';
-import githubDark from '../../assets/github-dark.svg';
-import linkedinLight from '../../assets/linkedin-light.svg';
-import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/cv.pdf';
-import { useContext } from 'react';
-import { ThemeContext } from '../../common/ThemeProvider';
+import styles from "./HeroStyles.module.css";
+import heroImg from "../../assets/kartik-img.png";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import instagramLight from "../../assets/instagram-light.svg";
+import instagramDark from "../../assets/instagram-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
+// import CV from "../../assets/cv.pdf";
+import { useContext } from "react";
+import { ThemeContext } from "../../common/ThemeProvider";
 
 function Hero() {
-  const useTheme = () => useContext(ThemeContext)
+  const useTheme = () => useContext(ThemeContext);
   const { theme, toggleTheme } = useTheme();
-  console.log(theme)
+  console.log(theme);
 
-  const themeIcon = theme === 'light' ? sun : moon;
-  const instagramIcon = theme === 'light' ? instagramLight : instagramDark;
-  const githubIcon = theme === 'light' ? githubLight : githubDark;
-  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+  const themeIcon = theme === "light" ? sun : moon;
+  const instagramIcon = theme === "light" ? instagramLight : instagramDark;
+  const githubIcon = theme === "light" ? githubLight : githubDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
 
   return (
     <section id="hero" className={styles.container}>
@@ -56,11 +56,16 @@ function Hero() {
           </a>
         </span>
         <p className={styles.description}>
-        Web Developer<br></br>
-        MCA Graduate<br></br>
-        Expert in Responsive Design.
+          MCA Graduate<br></br>
+          Full-Stack Enthusiast<br></br>
+          Skilled in UI & Responsiveness
         </p>
-        <a href={CV} download>
+        {/* <a href={CV} download> */}
+        <a
+          href="https://drive.google.com/file/d/1NfL6ZTc8KXR7MyF4SJPQ7K-cV0ebm129/view"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <button className="hover">Resume</button>
         </a>
       </div>
